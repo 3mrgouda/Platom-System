296d94c9db095ed8f4b75d64aaa0f3c6664bf167
3mR <amrgouda127@gmail.com>
2024-03-07 05:51:31 +0200

    make tables

--- src/components/controller/dashboard/crud.jsx/Memebers.jsx
+++ src/components/controller/dashboard/crud.jsx/Memebers.jsx
@@ -1,49 +1,39 @@
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 import React from 'react';
-import { App } from '../../../Firebase';
+
 
 
 export default function Memebers() {
-  const db = getFirestore(App);
-  // Collection ref
-  const colRef = collection(db, "members");
-
-  // get collection data
-  getDocs(colRef).then((snapshot) => {
-    let members = [];
-    snapshot.docs.forEach((doc) => {
-      members.push({ ...doc.data(), id: doc.id })
-    })
-    console.log(members)
-  })
-    .catch(err => {
-      console.log(err.message)
-    })
-
+ 
   return (
     <div className='w-full'>
       <div className=" w-full container lg:mx-auto">
         <table className='w-[80%] mx-auto my-4 text-left'>
-          <thead className='border' >
-            <tr> <th>ID</th>
+          <thead className='border ' >
+
+            <tr className='lg:text-2xl'>
+              <th>ID</th>
               <th>Name</th>
               <th>Team</th>
               <th>Event</th>
               <th>Actoins</th>
-              </tr>
+            </tr>
+
           </thead>
           <tbody>
-            <tr>
+
+
+            <tr className='border-b-2 lg:text-xl text-gray-700 capitalize'>
               <td>1</td>
               <td>Amr Mohamed</td>
               <td>flibbers</td>
               <td>collage</td>
               <td>
-                 <button>Update</button>
-              <button>Delet item</button>
+                <button className='mr-3'>Update</button>
+                <button>Delet item</button>
               </td>
-             
             </tr>
+
+
           </tbody>
         </table>
       </div>
--- src/components/controller/dashboard/crud.jsx/Teams.jsx
+++ src/components/controller/dashboard/crud.jsx/Teams.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Teams = () => {
   return (
-    <div>Teams</div>
+    <div className='w-full'>
+    <div className=" w-full container lg:mx-auto">
+      <table className='w-[80%] mx-auto my-4 text-left'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>flibbers</th>
+            <th>tribble</th>
+            <th>flibbers</th>
+            <th>tribble</th>
+            <th>flibbers</th>
+            <th>tribble</th>
+
+            </tr>
+        </thead>
+        </table>
+        </div>
+        </div>
   )
 }
 
