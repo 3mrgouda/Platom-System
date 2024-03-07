f26d49e59af4228ecd139a318b8e2f9a1e5822d3
3mR <amrgouda127@gmail.com>
2024-03-07 22:59:09 +0200

    connect with firebase

--- src/components/controller/dashboard/crud.jsx/Memebers.jsx
+++ src/components/controller/dashboard/crud.jsx/Memebers.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../../../Firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 
 
 export default function Memebers() {
- 
+  const [member, setMember] = useState([])
+  // Collection ref
+  const colRef = collection(db, "members");
+
+  // get collection data
+  useEffect(() => {
+    getDocs(colRef).then((snapshot) => {
+      var membersDocs = [];
+      snapshot.docs.forEach((doc) => {
+        membersDocs.push({ ...doc.data(), id: doc.id })
+      })
+      setMember(membersDocs)
+    })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }, [])
   return (
     <div className='w-full'>
       <div className=" w-full container lg:mx-auto">
@@ -12,26 +30,37 @@ export default function Memebers() {
 
             <tr className='lg:text-2xl'>
               <th>ID</th>
-              <th>Name</th>
+              <th>Frist name</th>
+              <th>Last name</th>
               <th>Team</th>
               <th>Event</th>
-              <th>Actoins</th>
+              <th>Update</th>
+              <th>Delete</th>
             </tr>
 
           </thead>
           <tbody>
 
-
-            <tr className='border-b-2 lg:text-xl text-gray-700 capitalize'>
-              <td>1</td>
-              <td>Amr Mohamed</td>
-              <td>flibbers</td>
-              <td>collage</td>
+            {
+              member.map((mem , i) => {
+                return (
+                  <tr className='border-b-2 lg:text-xl text-gray-700 capitalize'>
+              <td>{mem.iD}</td>
+              <td>{mem.Fname}</td>
+              <td>{mem.Lname}</td>
+              <td>{mem.Team}</td>
+              <td>{mem.Event}</td>
               <td>
-                <button className='mr-3'>Update</button>
-                <button>Delet item</button>
+                <button>Update</button>
+                </td>
+                <td>
+                <button>Delete</button>
               </td>
             </tr>
+                )
+              })
+            }
+            
 
 
           </tbody>
--- src/components/controller/dashboard/crud.jsx/Teams.jsx
+++ src/components/controller/dashboard/crud.jsx/Teams.jsx
@@ -1,26 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { db } from '../../../Firebase';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { FaPlus } from "react-icons/fa6";
+
+
 
 const Teams = () => {
-  return (
-    <div className='w-full'>
-    <div className=" w-full container lg:mx-auto">
-      <table className='w-[80%] mx-auto my-4 text-left'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>flibbers</th>
-            <th>tribble</th>
-            <th>flibbers</th>
-            <th>tribble</th>
-            <th>flibbers</th>
-            <th>tribble</th>
 
+  const [team, setTeam] = useState([]);
+  const [member, setMember] = useState([])
+  const [newTeamMember, setNewTeamMember] = useState([]);
+
+  // Collection reference
+  const colRef = collection(db, "teams");
+  const colref = collection(db, "members");
+
+  // Function to add a new team member
+  const addTeamMember = async () => {
+    try {
+      // Add data to Firestore
+      await addDoc(colRef, { Tname: newTeamMember }); // Add your desired data structure here
+      console.log("New team member added successfully!");
+      // Clear the input field
+      setNewTeamMember('');
+      location.reload();
+    } catch (error) {
+      console.error("Error adding new team member: ", error);
+    }
+  };
+
+  // Fetch collection data
+  useEffect(() => {
+    getDocs(colRef)
+      .then((snapshot) => {
+        const teamsDocs = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        setTeam(teamsDocs);
+      })
+      .catch(err => {
+        console.error("Error fetching team data: ", err);
+      });
+  }, []);
+
+  // get collection members
+  // useEffect(() => {
+  //   getDocs(colref).then((snapshot) => {
+  //     var membersDocs = [];
+  //     snapshot.docs.forEach((doc) => {
+  //       membersDocs.push({ ...doc.data(), id: doc.id })
+  //     })
+  //     setMember(membersDocs)
+  //   })
+  //     .catch(err => {
+  //       console.log(err.message)
+  //     })
+  // }, [])
+
+  return (
+    <>
+      <div className=" w-full container lg:mx-auto">
+
+        <div className='w-[80%] mx-auto flex items-center justify-evenly'> 
+          <input className='border-solid border-2 border-black rounded-md hover:scale-105 duration-200 px-2 py-1 hover:border-purple-500' value={newTeamMember} onChange={(e) => setNewTeamMember(e.target.value)} type="text" placeholder='Team Name' />
+          <button className='flex items-center text-xl border-solid border-2 shadow-md hover:shadow-sm shadow-black hover:shadow-purple-500 border-purple-500 rounded-md py-1 hover:border-black duration-200 hover:scale-105 px-2' onClick={addTeamMember}> <FaPlus/> Add New Team</button>
+        </div>
+
+        <table className='w-[80%] mx-auto my-4 text-left'>
+          <thead className='border ' >
+
+            <tr className='lg:text-2xl'>
+              <th>ID</th>
+              <th>Team name</th>
+              {/* <th>Members</th> */}
+              <th>Event</th>
+              <th>Update</th>
+              <th>Delete</th>
             </tr>
-        </thead>
+
+          </thead>
+          <tbody>
+
+            {
+              team.map((tem, i) => {
+                return (
+                  <tr key={i} className='border-b-2 lg:text-xl text-gray-700 capitalize'>
+                    <td>{i + 1}</td>
+                    <td>{tem.Tname}</td>
+                    {/* {
+                      member.map((mem) => {
+                        return (
+                          <td>{mem.Fname}</td>
+
+                        )
+                      })
+                    } */}
+                  </tr>
+                )
+              })
+            }
+          </tbody>
         </table>
-        </div>
-        </div>
-  )
+      </div>
+    </>
+  );
 }
 
 export default Teams
