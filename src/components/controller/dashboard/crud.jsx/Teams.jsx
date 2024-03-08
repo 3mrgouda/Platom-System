04e973fc9e5008a08008051a84d5a191b0a210a7
3mR <amrgouda127@gmail.com>
2024-03-08 21:11:40 +0200

    make delete for teams

--- src/components/controller/dashboard/crud.jsx/Teams.jsx
+++ src/components/controller/dashboard/crud.jsx/Teams.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../../Firebase';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
 import { FaPlus } from "react-icons/fa6";
+import { AiOutlineUsergroupDelete } from "react-icons/ai";
+
 
 
 
@@ -23,6 +25,7 @@ const Teams = () => {
       console.log("New team member added successfully!");
       // Clear the input field
       setNewTeamMember('');
+      setIsVisible
       location.reload();
     } catch (error) {
       console.error("Error adding new team member: ", error);
@@ -54,14 +57,31 @@ const Teams = () => {
   //       console.log(err.message)
   //     })
   // }, [])
+  const [isVisble, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    setIsVisible(!isVisble)
+  }
 
+
+
+  //delet items
+  const handleDelete = async (id, Tname) => {
+    try {
+      await deleteDoc(doc(db, 'teams', id))
+      alert(`" ${Tname} Team " has deleted succesfully`);
+      location.reload();
+
+    } catch (error) {
+      console.error('Error removing document: ', error);
+    }
+  };
   return (
     <>
       <div className=" w-full container lg:mx-auto">
 
-        <div className='w-[80%] mx-auto flex items-center justify-evenly'> 
+        <div id='add-setion' className='w-[80%] mx-auto  items-center justify-evenly hidden' style={{ display: isVisble ? 'flex' : isVisble }}>
           <input className='border-solid border-2 border-black rounded-md hover:scale-105 duration-200 px-2 py-1 hover:border-purple-500' value={newTeamMember} onChange={(e) => setNewTeamMember(e.target.value)} type="text" placeholder='Team Name' />
-          <button className='flex items-center text-xl border-solid border-2 shadow-md hover:shadow-sm shadow-black hover:shadow-purple-500 border-purple-500 rounded-md py-1 hover:border-black duration-200 hover:scale-105 px-2' onClick={addTeamMember}> <FaPlus/> Add New Team</button>
+          <button className='flex group items-center text-xl border-solid border-2 shadow-md hover:shadow-sm shadow-black hover:shadow-purple-500 border-purple-500 rounded-md py-1 hover:border-black duration-200 hover:scale-105 px-2' onClick={addTeamMember}> <FaPlus /> Add New Team</button>
         </div>
 
         <table className='w-[80%] mx-auto my-4 text-left'>
@@ -74,6 +94,10 @@ const Teams = () => {
               <th>Event</th>
               <th>Update</th>
               <th>Delete</th>
+              <th>
+                <button onClick={() => toggleVisibility()} className='flex items-center text-xl bg-purple-500 rounded-md  px-2 lg:mt-[3px] font-bold'> <FaPlus />Add</button>
+
+              </th>
             </tr>
 
           </thead>
@@ -81,8 +105,9 @@ const Teams = () => {
 
             {
               team.map((tem, i) => {
+
                 return (
-                  <tr key={i} className='border-b-2 lg:text-xl text-gray-700 capitalize'>
+                  <tr key={i} className='border-b-2 lg:text-xl text-gray-700 capitalize hover:bg-purple-500 cursor-pointer hover:text-black hover:scale-105 duration-150'>
                     <td>{i + 1}</td>
                     <td>{tem.Tname}</td>
                     {/* {
@@ -93,6 +118,9 @@ const Teams = () => {
                         )
                       })
                     } */}
+                    <td>event</td>
+                    <td>update</td>
+                    <td><button onClick={() => handleDelete(tem.id, tem.Tname)} className='ml-5'><AiOutlineUsergroupDelete className='hover:scale-150 hover:bg-red-600 hover:rounded-md duration-200' /></button></td>
                   </tr>
                 )
               })
